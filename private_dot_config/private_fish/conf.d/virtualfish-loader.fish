168ee5e19bb35a90d9ba4a67c5ed916ace7059f3
Ryan Eikanger <reikanger@gmail.com>
2025-05-13 06:45:37 +0000

    initial commit

---
+++ private_dot_config/private_fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,7 @@
+set -g VIRTUALFISH_VERSION 2.5.9
+set -g VIRTUALFISH_PYTHON_EXEC /home/reika/.local/share/pipx/venvs/virtualfish/bin/python
+source /home/reika/.local/share/pipx/venvs/virtualfish/lib/python3.13/site-packages/virtualfish/virtual.fish
+source /home/reika/.local/share/pipx/venvs/virtualfish/lib/python3.13/site-packages/virtualfish/compat_aliases.fish
+source /home/reika/.local/share/pipx/venvs/virtualfish/lib/python3.13/site-packages/virtualfish/projects.fish
+source /home/reika/.local/share/pipx/venvs/virtualfish/lib/python3.13/site-packages/virtualfish/environment.fish
+emit virtualfish_did_setup_plugins
